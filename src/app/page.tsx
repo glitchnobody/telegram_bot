ed5c130f1449d91c0febd5fc3c9612e6edc2c992
siddhant <siddhantvij001@gmail.com>
2024-03-22 04:04:49 +0530

    second bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isValid, setValid] = useState(false);
+
+  useEffect(() => {
+    window.Telegram.WebApp.ready() && setValid(true);
+  }, []);
+
+  if (!isValid) {
+    return null;
+  }
+
   return (
     <main className="">
       sdas
