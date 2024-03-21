4adefcc3135015133a6a237fe8aa6abc0e881340
siddhant <siddhantvij001@gmail.com>
2024-03-22 04:22:25 +0530

    fix for loding

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,10 +2,10 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "6703173997:AAEFCCyIlRBprLUxUtvQYepJRKq__3UApOc";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://starlit-raindrop-2d0fd2.netlify.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+  ctx.reply("Hi! lets get you started Click the Get started", {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,94 @@
 "use client";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [isValid, setValid] = useState(false);
+import { useBackButton, useInitData, useMainButton } from "@tma.js/sdk-react";
+import { useEffect, useMemo, useState } from "react";
+
+function MainButtonTest() {
+  const mainButton = useMainButton();
+  const backButton = useBackButton();
+
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    window.Telegram.WebApp.ready() && setValid(true);
+    const onMainButtonClick = () => setCount((prevCount) => prevCount + 1);
+    const onBackButtonClick = () => setCount((prevCount) => prevCount - 1);
+
+    mainButton.enable().show();
+    mainButton.on("click", onMainButtonClick);
+    backButton.on("click", onBackButtonClick);
+
+    return () => {
+      mainButton.off("click", onMainButtonClick);
+      mainButton.hide();
+      backButton.off("click", onBackButtonClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!isValid) {
-    return null;
-  }
+  useEffect(() => {
+    mainButton.setText(`Count is ${count}`);
+  }, [mainButton, count]);
+
+  useEffect(() => {
+    if (count === 0) {
+      backButton.hide();
+      return;
+    }
+    backButton.show();
+  }, [backButton, count]);
+
+  return null;
+}
+
+/**
+ * Displays current application init data.
+ */
+function InitData() {
+  const initData = useInitData();
+
+  const initDataJson = useMemo(() => {
+    if (!initData) {
+      return "Init data is empty.";
+    }
+    const {
+      authDate,
+      chat,
+      hash,
+      canSendAfter,
+      queryId,
+      receiver,
+      user,
+      startParam,
+    } = initData;
+
+    return JSON.stringify(
+      {
+        authDate,
+        chat,
+        hash,
+        canSendAfter,
+        queryId,
+        receiver,
+        user,
+        startParam,
+      },
+      null,
+      " ",
+    );
+  }, [initData]);
 
   return (
-    <main className="">
-      sdas
-      <div>ssd</div>
-    </main>
+    <pre>
+      <code>{initDataJson}</code>
+    </pre>
+  );
+}
+
+export default function Home() {
+  return (
+    <>
+      <MainButtonTest />
+      <InitData />
+    </>
   );
 }
